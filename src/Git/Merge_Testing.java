93988c74ccaf9a2746cafaf792969dd3b2444e1e
Eryadu <yadavgupta07@gmail.com>
2025-02-04 21:25:30 -0500

    Merge_Testing

---
+++ src/Git/Merge_Testing.java
@@ -0,0 +1,21 @@
+package Git;
+
+public class Merge_Testing {
+    static String name = "Yadav";
+    static int add_no = 1;
+
+   // Merge_Testing mer = new Merge_Testing();
+
+    public static void main(String[] args) {
+
+        System.out.println(Merge_Testing.name);
+        Merge_Testing();
+
+    }
+    static void Merge_Testing(){
+
+        System.out.println(Merge_Testing.name);
+        System.out.println(Merge_Testing.add_no);
+    }
+
+}
