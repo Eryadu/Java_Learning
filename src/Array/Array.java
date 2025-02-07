95c36fa1725754dc2aa7fccfcd0c84b5c9602035
Eryadu <yadavgupta07@gmail.com>
2025-02-06 20:33:47 -0500

    array

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,20 +1,33 @@
 package Array;
 
 import javax.sound.midi.Soundbank;
+import javax.xml.bind.Element;
 import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
 
         Scanner scanner= new Scanner(System.in);
-        int[] name = new int[10];
+        int[] name = new int[2];
         System.out.println("Enter the elements of Array ..");
         for (int i =0; i< name.length; i++)
         {
             name[i]=scanner.nextInt();
         }
-        for (int i=0; i<name.length; i++) {
-            System.out.println(name[i]);
+
+        // enhanced for loop
+        // for every integer element in the array, print that element
+        for(int element : name) // enhanced method to print array .. you can take any variable in place of element i.e a,b,c
+        {
+            System.out.println(element);
         }
+       /* for(int ie : name) // enhanced method to print array
+        {
+            System.out.println(ie);
+        }
+        */
+   //     for (int i=0; i<name.length; i++) { // second methode to print array
+   //         System.out.println(name[i]);
+     //   }
     }
 }
--- src/Array/array.md
+++ src/Array/array.md
@@ -1,5 +1,13 @@
 Array --- > similar type of data/ Homogenous
 store at continous memory locations
+Array is of fixed size.
+
+// Array is non-primitive data type.
+
 
 //datatype[] ref = new datatype[size]
 int[] arr = new int[];
+int[] arr = {10,20,30,40,50}
+
+// 10 20 30 40 50 -------> Elements of array.
+// 0  1  2  3  4 -------> index of array.
